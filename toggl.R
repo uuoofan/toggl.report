83aa5cae44cf1588dace69352f46f6f2e87963e9
uuoofan <uuoofan@gmail.com>
2017-05-08 23:59:40 -0400

    05/08/2017

--- toggl.R
+++ toggl.R
@@ -2,14 +2,15 @@
 library(pdftools)
 library(lubridate)
 library(knitr)
-tmp = pdf_text("./Toggl_projects_2017-05-07_to_2017-05-07.pdf")
+fname = "./Toggl_projects_2017-05-08_to_2017-05-08"
+tmp = pdf_text(paste0(fname, ".pdf"))
 time.entry = unlist(strsplit(tmp[2], "\n"))
 time.entry = time.entry[-1]
 time.entry = time.entry[-length(time.entry)]
 
 # cat.entry = grep("^\\S", time.entry, value = TRUE)
 # cats = gsub("\\s+$", "", gsub("(.+)(\\d+:\\d+:\\d+)", "\\1", cat.entry))
-cats.csv = read.csv("./Toggl_projects_2017-05-07_to_2017-05-07.csv", as.is = TRUE)
+cats.csv = read.csv(paste0(fname, ".csv"), as.is = TRUE)
 cats = cats.csv$Project
 cat.id = grep("^\\S", time.entry)
 cat.no = c(diff(cat.id), length(time.entry) + 1 - tail(cat.id, 1))
